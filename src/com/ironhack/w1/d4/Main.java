9fc18b843ff9526cc8ef734683757724f04ec1b0
Ignacio Talento <ignaciotalento@gmail.com>
2025-04-25 21:04:49 +0200

    big numbers

--- src/com/ironhack/w1/d4/Main.java
+++ src/com/ironhack/w1/d4/Main.java
@@ -1,9 +1,12 @@
 package com.ironhack.w1.d4;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class Main {
     public static void main(String[] args) {
 
-//        Abastract classes & interfaces
+//        Abstract classes & interfaces
 
         WebPlayer webPlayer = new WebPlayer(0.5, 1);
         ApplePlayer applePlayer = new ApplePlayer(0.2, 2);
@@ -28,6 +31,32 @@ public class Main {
         System.out.println(androidPlayer.getVolume());
 
         System.out.println(androidPlayer.share());
+
+//        Big decimal (Java class)
+
+        System.out.println(1.2 - 1.0);
+
+        BigDecimal bd1 = new BigDecimal("1.2385");
+        BigDecimal bd2 = new BigDecimal("1.02346");
+        System.out.println(bd1.subtract(bd2));
+        System.out.println(bd1.add(bd2));
+        System.out.println(bd1.multiply(bd2));
+        System.out.println(bd1.divide(bd2, RoundingMode.HALF_EVEN));
+        System.out.println(bd1.setScale(2, RoundingMode.HALF_EVEN));
+        System.out.println(bd1.compareTo(bd2));
+        System.out.println(bd2.compareTo(bd1));
+        System.out.println(bd1.compareTo(bd1));
+
+        if (bd1.compareTo(bd2) > 0) {
+            System.out.println("bd1 is greater than bd2");
+        } else if (bd1.compareTo(bd2) < 0) {
+            System.out.println("bd1 is not greater than bd2");
+        } else if (bd1.compareTo(bd2) == 0){
+            System.out.println("bd1 is equal to bd2");
+        }
+
+
+
 
     }
 }
